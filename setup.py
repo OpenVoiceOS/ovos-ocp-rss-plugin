d63513bd7561c7bdc93c99a7156147496d79c7eb
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-09-11 04:27:22 +0100

    feat:semver (#3)

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ CONFIG_ENTRY_POINT = f'{PLUGIN_NAME}.config = {PLUGIN_PKG}:{PLUGIN_CONFIGS}'
 
 def get_version():
     """ Find the version of the package"""
-    version = None
     version_file = os.path.join(BASEDIR, PLUGIN_PKG, 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
